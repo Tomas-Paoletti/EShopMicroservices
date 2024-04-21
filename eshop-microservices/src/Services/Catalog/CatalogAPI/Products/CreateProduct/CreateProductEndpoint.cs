704a39a95fb3879ddda29372e6ecd025fb027e4c
Tomas Paoletti <81266504+Tomas-Paoletti@users.noreply.github.com>
2024-04-21 04:02:07 -0300

    fiish endpoint createProduct

---
+++ eshop-microservices/src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CatalogAPI.Products.CreateProduct
+{
+
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>(); //mapping object  to command
+                var result = await sender.Send(command); //send command to handler
+                var response = result.Adapt<CreateProductResponse>(); //mapping result to response
+                return Results.Created($"/products/{response.Id}", response);
+            })
+                .WithName("CreateProduct") // Sets the name of the endpoint
+                .Produces<CreateProductResponse>(StatusCodes.Status201Created) // Specifies the response type and status code
+                .Produces(StatusCodes.Status400BadRequest) // Specifies the status code for a bad request
+                .WithSummary("Creates a new product") // Adds a summary for the endpoint
+                .WithDescription("Creates a new product in the catalog"); // Adds a description for the endpoint
+        }
+    }
+}
---
+++ eshop-microservices/src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogAPI.Models;
+
+using System.Windows.Input;
+
+namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price):ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create product from command object 
+
+            //save product to database
+
+
+                //return CreateProductResult result
+
+
+            var product = new Product
+            {
+                
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+            throw new NotImplementedException();
+        }
+    }
+}
